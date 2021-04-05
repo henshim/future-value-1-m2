4ca0ae2e6b4868959ae48e6dfa231262ff86e0ae
henshim <hieume113@gmail.com>
2021-04-05 08:25:02 +0700

    bai1

--- index.php
+++ index.php
@@ -12,17 +12,15 @@
     <input type="number" placeholder="inventment amount" name="amount">
     <input type="number" placeholder="yearly interest rate" name="rate">
     <input type="number" placeholder="number of years" name="year">
-    <button>Calculate</button>
+    <button type="submit">Calculate</button>
 </form>
 <?php
-for ($i = 0; i < count(); $i++) {
-    if ($_SERVER = ['METHOD REQUIRED'] == 'POST') {
-        $amount = $_POST['amount'];
-        $rate = $_POST['rate'];
-        $year = $_POST['year'];
-        $result=$amount*((1+$rate/100)**$year);
-        echo 'future value after '.$year.'year is '.$result;
-    }
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $amount = $_POST['amount'];
+    $rate = $_POST['rate'];
+    $year = $_POST['year'];
+    $result = $amount * ((1 + $rate / 100) * $year);
+    echo 'future value after ' . $year . ' year is ' . $result;
 }
 ?>
 </body>
